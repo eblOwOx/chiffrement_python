0df66803f46ff120d47bfb0bfc7f7777b304f57c
eblOwOx <amaurymplom@proton.me>
2025-09-26 12:13:57 +0200

    Ajout de plus de tests pour l'AES

--- main.py
+++ main.py
@@ -30,12 +30,15 @@ monAes.encrypt_file(normal, crypt) #Chiffrement du fichier nommé normal.txt dan
 print("Le contenu du fichier normal.txt dans file/aes/ a ete chiffre en un nouveau fichier nomme cryptaes.txt")
 monAes.decrypt_file(crypt, decrypt) #Déchiffrement du fichier crypt.txt dans un fichier nommé 
 print("Le fichier cryptaes.txt a ete dechiffre en un nouveau fichier nomme decryptaes.txt")
-
+#reprise de la clef aes de quelqu'un d'autre pour dechiffrer un message
+print("\nDechiffrement d'un message d'une autre personne avec une cle aes...")
+monAes.load_aes_key_from_file('files/aes/import/aes_keys.bin')
+monAes.decrypt_file('files/aes/import/test_crypting.txt', 'files/aes/import/decrypt.txt')
 
 #Partie RSA
 print("\n\n################################## PARTIE RSA ####################################")
-fichPublic = 'files/rsa/fichPublic' # Localisation de la clef publique
-fichPrive = 'files/rsa/fichPrive' # Localisation de la clef privee
+fichPublic = 'files/rsa/fichPublic.txt' # Localisation de la clef publique
+fichPrive = 'files/rsa/fichPrive.txt' # Localisation de la clef privee
 fichTxtChiff = 'files/rsa/fichTxtChiff.txt' # Localisation du fichier texte chiffre
 
 #1ère partie
@@ -58,3 +61,15 @@ print("Message chiffre : ", messageChif, "\n")
 print("Dechiffrement du fichier texte...")
 message2Dechif = monRsa.dechiffre_fichier(fichTxtChiff) # Déchiffrement du fichier
 print("Message dechiffre : ", message2Dechif, "\n")
+
+#3ème partie
+#messageAEnvoyer = 'files/rsa/messageChiffre.txt'
+#monRsa.chargement_clefs(fichPublic, fichPrive) # Chargement de la paire de clefs
+#message3 = "sudo make me a sandwich - xkcd 2006"
+#print("Chiffement du message dans un fichier texte : ", message3)
+#monRsa.chiffre_dans_fichier(message3, messageAEnvoyer) # Chiffrement du message dans un fichier
+#print("Message chiffre a envoyer sur un autre pc : ", messageAEnvoyer, "\n")
+#Reprise de la clef publique et du message chiffre de quelqu'un d'autre
+#clefPubImport = 'files/rsa/import/'
+#print("\nChargement de la clef publique de quelqu'un d'autre...")
+#monRsa.chargement_clef_publique()
